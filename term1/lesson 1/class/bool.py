7a9b345e63da8c706296f86b4f39367e93619bb2
tk_wang <wangshenze20040220@outlook.com>
2024-03-01 11:22:37 +0800

    bool 运算 与 input 方法

---
+++ term1/lesson 1/class/bool.py
@@ -0,0 +1,13 @@
+print(bool("abc"))
+print(bool(123))
+print(bool(False))
+print(bool(True))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(["apple", "cherry", "banana"]))
+
+# 不同于 JavaScript 特殊记忆
+print(bool(()))
+print(bool([]))
+print(bool({}))
